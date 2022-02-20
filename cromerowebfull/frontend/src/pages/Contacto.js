af232ee8530f3d97de176a8269a7b0528c36d95f
Claudio Romero <clau_sr511@hotmail.com>
2022-02-20 20:24:53 -0300

    heroku

--- cromerowebfull/frontend/src/pages/Contacto.js
+++ cromerowebfull/frontend/src/pages/Contacto.js
@@ -1,43 +1,83 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import '../styles/components/pages/Contacto.css'
 
 const Contacto = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value // forma dinamica
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
     return (
         <main className="contenedor_principal holder">
-            <div className="img_bienvenida">
-                <img className="img_bienvenida" src="imagenes/contacto/contacto.jpg" alt="" width="100%" />
-            </div>
+
             <div className="contacto holder">
                 <div className="contactoleft">
-                    <h2>Contacto Rapido</h2>
-                    <form action="" className="formulario">
+                    {/* <h2>Contacto Rapido</h2> */}
+                    <form className="formulario loginBox" onsubmit={handleSubmit}>
                         <p>
-                            <label for="">Nombre</label>
-                            <input type="text" />
+                            <label>Nombre</label>
+                            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
                         </p>
                         <p>
-                            <label for="">Email</label>
-                            <input type="email" />
+                            <label>Email</label>
+                            <input type="text" name="email" value={formData.email} onChange={handleChange}/>
                         </p>
                         <p>
-                            <label for="">Telefono</label>
-                            <input type="text" />
+                            <label>Telefono</label>
+                            <input type="text" name="telefono" value={formData.telefono} onChange={handleChange}/>
                         </p>
                         <p>
-                            <label for="comentario">Comentario</label>
-                            <textarea name="" id="comentario" cols="30" rows="10"></textarea>
+                            <label>Comentario</label>
+                            <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}/>
                         </p>
-                        <p className="acciones"><input type="submit" value="Enviar"/></p>
+                        {sending ? <p>Enviando...</p> : null}
+                        {msg ? <p>{msg}</p> : null}
+                        <p className="centrar"><input type="submit" value="Enviar" /></p>
                     </form>
+
                 </div>
                 <div className="contactoright">
                     <h2>Otras vias de contacto</h2>
                     <ul>
                         <li>Telefonos 4578-7456</li>
-                        <li>Email: info@cromerowebfull.com</li>
+                        <li>Email: clau_sr511@hotmail.com</li>
                         <li>Facebook</li>
                         <li>Instagram</li>
                     </ul>
+                    <div className="img_bienvenida">
+                <img className="img_bienvenida" src="imagenes/contacto/contacto.jpg" alt="" width="100%" />
+            </div>
                 </div>
+                
             </div>
         </main>
 
